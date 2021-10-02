eb66760da219ce65236402b11c23d899a7d52482
Anthony Chu <anthony@anthonychu.ca>
2021-10-01 17:04:27 -0700

    Update file

--- blazor-wasm-aot.sh
+++ blazor-wasm-aot.sh
@@ -1,5 +1,9 @@
-git clone https://github.com/anthonychu/ConwayBlazor.git
-cd ConwayBlazor
-git checkout aot-net6-rc1
-cd Client
+# git clone https://github.com/anthonychu/PictureFixer.git
+# cd PictureFixer
+# git checkout net6rc1
+# cd PictureFixer/Client
+
+mkdir /foo
+cd /foo
+touch foo.csproj
 oryx build . -o /bin/staticsites/ss-oryx/app --manifest-dir /bin/staticsites/ss-oryx/app-manifest -p prune_dev_dependencies -p require_build -p disable_recursive_lookup=true -p ORYX_APP_TYPE="static-sites"
