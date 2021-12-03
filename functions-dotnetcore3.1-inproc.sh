f82e709ecbcecf4c8f8762824c86af92a13fc8f9
Anthony Chu <anthony@anthonychu.ca>
2021-12-02 21:56:05 -0800

    Update functions-dotnetcore3.1-inproc.sh

--- functions-dotnetcore3.1-inproc.sh
+++ functions-dotnetcore3.1-inproc.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/staticwebdev/blazor-starter.git
-git checkout netcore3.1
 cd blazor-starter/Api
+git checkout netcore3.1
 
 oryx build . -o /bin/staticsites/ss-oryx/api --manifest-dir /bin/staticsites/ss-oryx/api-manifest -p prune_dev_dependencies -p packagedir=.python_packages/lib/site-packages
 
